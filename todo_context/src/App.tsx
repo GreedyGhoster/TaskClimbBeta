18f925bfda4e848db76fd705178c23d4b7b5dd8f
IuanPR <103894461+IuanPR@users.noreply.github.com>
2023-09-12 21:43:30 +0300

    Added a limit on the screen width

--- todo_context/src/App.tsx
+++ todo_context/src/App.tsx
@@ -1,6 +1,17 @@
 import "./global.css";
 import { AppRouter } from "./routes";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export default function App() {
-  return <AppRouter />;
+  const matches = useMediaQuery("(min-width:1024px)");
+
+  return (
+    <>
+      {matches ? (
+        <AppRouter />
+      ) : (
+        <h1>Try opening on a wider screen (1024px and higher)</h1>
+      )}
+    </>
+  );
 }
